43c1f32b7f5bd700fc57a7db1edd4e54706cc18d
John McNamara <jmcnamara@cpan.org>
2010-08-30 01:50:18 +0100

    Added initial XLSX core.xml code and tests.

---
+++ lib/Excel/XLSX/Writer/Container/Core.pm
@@ -0,0 +1,318 @@
+package Excel::XLSX::Writer::Container::Core;
+
+###############################################################################
+#
+# Core - A class for writing the Excel XLSX core.xml file.
+#
+# Used in conjunction with Excel::XLSX::Writer
+#
+# Copyright 2000-2010, John McNamara, jmcnamara@cpan.org
+#
+# Documentation after __END__
+#
+
+# perltidy with the following options: -mbl=2 -pt=0 -nola
+
+use 5.010000;
+use strict;
+use warnings;
+use Exporter;
+use Carp;
+use XML::Writer;
+
+our @ISA     = qw(Exporter);
+our $VERSION = '0.01';
+
+
+###############################################################################
+#
+# Public and private API methods.
+#
+###############################################################################
+
+
+###############################################################################
+#
+# new()
+#
+# Constructor.
+#
+sub new {
+
+    my $class = shift;
+
+    my $self = {
+        _writer            => undef,
+        _creator           => '',
+        _modifier          => '',
+        _creation_date     => '2010-01-01T00:00:00Z',
+        _modification_date => '2010-01-01T00:00:00Z',
+    };
+
+    bless $self, $class;
+
+    return $self;
+}
+
+
+###############################################################################
+#
+# _assemble_xml_file()
+#
+# Assemble and write the XML file.
+#
+sub _assemble_xml_file {
+
+    my $self = shift;
+
+    return unless $self->{_writer};
+
+    $self->_write_xml_declaration;
+    $self->_write_cp_core_properties();
+}
+
+
+###############################################################################
+#
+# _set_xml_writer()
+#
+# Set the XML::Writer for the object.
+#
+sub _set_xml_writer {
+
+    my $self     = shift;
+    my $filename = shift;
+
+    my $output = new IO::File( $filename, 'w' );
+    my $writer = new XML::Writer( OUTPUT => $output );
+
+    $self->{_writer} => $writer;
+}
+
+
+###############################################################################
+#
+# _set_creator()
+#
+# Set the document creator.
+#
+sub _set_creator {
+    my $self    = shift;
+    my $creator = shift;
+
+    $self->{_creator} = $creator;
+}
+
+
+###############################################################################
+#
+# _set_modifier()
+#
+# Set the document modifier.
+#
+sub _set_modifier {
+    my $self     = shift;
+    my $modifier = shift;
+
+    $self->{_modifier} = $modifier;
+}
+
+
+###############################################################################
+#
+# _set_creation_date()
+#
+# Set the document creation date.
+#
+sub _set_creation_date {
+    my $self          = shift;
+    my $creation_date = shift;
+
+    $self->{_creation_date} = $creation_date;
+}
+
+
+###############################################################################
+#
+# _set_modification_date()
+#
+# Set the document modification date.
+#
+sub _set_modification_date {
+    my $self              = shift;
+    my $modification_date = shift;
+
+    $self->{_modification_date} = $modification_date;
+}
+
+
+###############################################################################
+#
+# Internal methods.
+#
+###############################################################################
+
+
+###############################################################################
+#
+# XML writing methods.
+#
+###############################################################################
+
+
+###############################################################################
+#
+# _write_xml_declaration()
+#
+# Write the XML declaration.
+#
+sub _write_xml_declaration {
+
+    my $self       = shift;
+    my $writer     = $self->{_writer};
+    my $encoding   = 'UTF-8';
+    my $standalone = 1;
+
+    $writer->xmlDecl( $encoding, $standalone );
+}
+
+
+###############################################################################
+#
+# _write_cp_core_properties()
+#
+# Write the <cp:coreProperties> element.
+#
+sub _write_cp_core_properties {
+
+    my $self = shift;
+    my $xmlns_cp =
+      'http://schemas.openxmlformats.org/package/2006/metadata/core-properties';
+    my $xmlns_dc       = 'http://purl.org/dc/elements/1.1/';
+    my $xmlns_dcterms  = 'http://purl.org/dc/terms/';
+    my $xmlns_dcmitype = 'http://purl.org/dc/dcmitype/';
+    my $xmlns_xsi      = 'http://www.w3.org/2001/XMLSchema-instance';
+
+    my @attributes = (
+        'xmlns:cp'       => $xmlns_cp,
+        'xmlns:dc'       => $xmlns_dc,
+        'xmlns:dcterms'  => $xmlns_dcterms,
+        'xmlns:dcmitype' => $xmlns_dcmitype,
+        'xmlns:xsi'      => $xmlns_xsi,
+    );
+
+    $self->{_writer}->startTag( 'cp:coreProperties', @attributes );
+    $self->_write_dc_creator();
+    $self->_write_cp_last_modified_by();
+    $self->_write_dcterms_created();
+    $self->_write_dcterms_modified();
+    $self->{_writer}->endTag( 'cp:coreProperties' );
+}
+
+
+###############################################################################
+#
+# _write_dc_creator()
+#
+# Write the <dc:creator> element.
+#
+sub _write_dc_creator {
+
+    my $self = shift;
+    my $data = $self->{_creator};
+
+    $self->{_writer}->dataElement( 'dc:creator', $data );
+}
+
+
+###############################################################################
+#
+# _write_cp_last_modified_by()
+#
+# Write the <cp:lastModifiedBy> element.
+#
+sub _write_cp_last_modified_by {
+
+    my $self = shift;
+    my $data = $self->{_modifier};
+
+    $self->{_writer}->dataElement( 'cp:lastModifiedBy', $data );
+}
+
+
+###############################################################################
+#
+# _write_dcterms_created()
+#
+# Write the <dcterms:created> element.
+#
+sub _write_dcterms_created {
+
+    my $self     = shift;
+    my $data     = $self->{_creation_date};
+    my $xsi_type = 'dcterms:W3CDTF';
+
+    my @attributes = ( 'xsi:type' => $xsi_type, );
+
+
+    $self->{_writer}->dataElement( 'dcterms:created', $data, @attributes );
+}
+
+
+###############################################################################
+#
+# _write_dcterms_modified()
+#
+# Write the <dcterms:modified> element.
+#
+sub _write_dcterms_modified {
+
+    my $self     = shift;
+    my $data     = $self->{_modification_date};
+    my $xsi_type = 'dcterms:W3CDTF';
+
+    my @attributes = ( 'xsi:type' => $xsi_type, );
+
+
+    $self->{_writer}->dataElement( 'dcterms:modified', $data, @attributes );
+}
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Core - A class for writing the Excel XLSX core.xml file.
+
+=head1 SYNOPSIS
+
+See the documentation for L<Excel::XLSX::Writer>.
+
+=head1 DESCRIPTION
+
+This module is used in conjunction with L<Excel::XLSX::Writer>.
+
+=head1 AUTHOR
+
+John McNamara jmcnamara@cpan.org
+
+=head1 COPYRIGHT
+
+© MM-MMX, John McNamara.
+
+All Rights Reserved. This module is free software. It may be used, redistributed and/or modified under the same terms as Perl itself.
+
+=head1 LICENSE
+
+Either the Perl Artistic Licence L<http://dev.perl.org/licenses/artistic.html> or the GPL L<http://www.opensource.org/licenses/gpl-license.php>.
+
+=head1 DISCLAIMER OF WARRANTY
+
+See the documentation for L<Excel::XLSX::Writer>.
+
+=cut
---
+++ t/container/core/core.t
@@ -0,0 +1,86 @@
+################################################################################
+#
+# Tests for Excel::XLSX::Writer::Container::Core methods.
+#
+# reverse('©'), September 2010, John McNamara, jmcnamara@cpan.org
+#
+
+use strict;
+use warnings;
+use Excel::XLSX::Writer::Container::Core;
+use XML::Writer;
+
+use Test::More tests => 1;
+
+################################################################################
+#
+# Tests setup.
+#
+my $expected;
+my $caption;
+
+open my $got_fh, '>', \my $got or die "Failed to open filehandle: $!";
+
+my $obj    = Excel::XLSX::Writer::Container::Core->new();
+my $writer = new XML::Writer( OUTPUT => $got_fh );
+
+$obj->{_writer} = $writer;
+
+################################################################################
+#
+# Test the _assemble_xml_file() method.
+#
+$caption = " \tContentTypes: _assemble_xml_file()";
+
+$obj->_set_creator('A User');
+$obj->_set_modifier('Another User');
+$obj->_set_creation_date('2010-01-02T00:00:00Z');
+$obj->_set_modification_date('2010-01-03T00:00:00Z');
+$obj->_assemble_xml_file();
+
+$expected = _expected_to_aref();
+$got      = _got_to_aref( $got );
+
+use Test::Differences;
+eq_or_diff( $got, $expected, $caption,  { context => 0 } );
+#is_deeply( $got, $expected, $caption );
+
+################################################################################
+#
+# Utility functions used by tests.
+#
+sub _expected_to_aref {
+
+    my @data;
+
+    while ( <DATA> ) {
+        next unless /\S/;
+        chomp;
+        s{/>$}{ />};
+        s{^\s+}{};
+        push @data, $_;
+    }
+
+    return \@data;
+}
+
+sub _got_to_aref {
+
+    my $xml_str = shift;
+
+    $xml_str =~ s/\n//;
+
+    # Split the XML into chunks at element boundaries.
+    my @data = split /(?<=>)(?=<)/, $xml_str;
+
+    return \@data;
+}
+
+__DATA__
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <dc:creator>A User</dc:creator>
+  <cp:lastModifiedBy>Another User</cp:lastModifiedBy>
+  <dcterms:created xsi:type="dcterms:W3CDTF">2010-01-02T00:00:00Z</dcterms:created>
+  <dcterms:modified xsi:type="dcterms:W3CDTF">2010-01-03T00:00:00Z</dcterms:modified>
+</cp:coreProperties>
