8e79f612e730ab5f9fc5aafb62e72a1bdcff8711
John McNamara <jmcnamara@cpan.org>
2010-09-18 20:03:07 +0100

    Refactored XML writer code up into superclass.

---
+++ lib/Excel/XLSX/Writer/Package/XMLwriter.pm
@@ -0,0 +1,121 @@
+package Excel::XLSX::Writer::Package::XMLwriter;
+
+###############################################################################
+#
+# XMLwriter - A base class for the Excel::XLSX::Writer writer clases.
+#
+# Used in conjunction with Excel::XLSX::Writer
+#
+# Copyright 2000-2010, John McNamara, jmcnamara@cpan.org
+#
+# Documentation after __END__
+#
+
+# perltidy with the following options: -mbl=2 -pt=0 -nola
+
+use 5.010000;
+use strict;
+use warnings;
+use Exporter;
+use Carp;
+use XML::Writer;
+use IO::File;
+
+our @ISA     = qw(Exporter);
+our $VERSION = '0.01';
+
+
+###############################################################################
+#
+# new()
+#
+# Constructor.
+#
+sub new {
+
+    my $class = shift;
+
+    my $self = { _writer => undef };
+
+    bless $self, $class;
+
+    return $self;
+}
+
+
+###############################################################################
+#
+# _set_xml_writer()
+#
+# Set the XML::Writer for the object.
+#
+sub _set_xml_writer {
+
+    my $self     = shift;
+    my $filename = shift;
+
+    my $output = new IO::File( $filename, 'w' );
+    croak "Couldn't open file $filename for writing.\n" unless $output;
+
+    my $writer = new XML::Writer( OUTPUT => $output );
+    croak "Couldn't create XML::Writer for $filename.\n" unless $writer;
+
+    $self->{_writer} = $writer;
+}
+
+
+###############################################################################
+#
+# _write_xml_declaration()
+#
+# Write the XML declaration.
+#
+sub _write_xml_declaration {
+
+    my $self       = shift;
+    my $writer     = $self->{_writer};
+    my $encoding   = 'UTF-8';
+    my $standalone = 1;
+
+    $writer->xmlDecl( $encoding, $standalone );
+}
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+XMLwriter - A base class for the Excel::XLSX::Writer writer clases.
+
+=head1 SYNOPSIS
+
+See the documentation for L<Excel::XLSX::Writer>.
+
+=head1 DESCRIPTION
+
+This module is used in conjunction with L<Excel::XLSX::Writer>. This module uses L<XML::Writer>.
+
+=head1 AUTHOR
+
+John McNamara jmcnamara@cpan.org
+
+=head1 COPYRIGHT
+
+© MM-MMX, John McNamara.
+
+All Rights Reserved. This module is free software. It may be used, redistributed and/or modified under the same terms as Perl itself.
+
+=head1 LICENSE
+
+Either the Perl Artistic Licence L<http://dev.perl.org/licenses/artistic.html> or the GPL L<http://www.opensource.org/licenses/gpl-license.php>.
+
+=head1 DISCLAIMER OF WARRANTY
+
+See the documentation for L<Excel::XLSX::Writer>.
+
+=cut
---
+++ t/package/xml_writer/sub_write_xml_declaration.t
@@ -0,0 +1,42 @@
+###############################################################################
+#
+# Tests for Excel::XLSX::Writer::Package::XMLwriter methods.
+#
+# reverse('©'), September 2010, John McNamara, jmcnamara@cpan.org
+#
+
+use strict;
+use warnings;
+use Excel::XLSX::Writer::Package::XMLwriter;
+use XML::Writer;
+
+use Test::More tests => 1;
+
+###############################################################################
+#
+# Tests setup.
+#
+my $expected;
+my $caption;
+
+open my $got_fh, '>', \my $got or die "Failed to open filehandle: $!";
+
+my $obj    = Excel::XLSX::Writer::Package::XMLwriter->new();
+my $writer = new XML::Writer( OUTPUT => $got_fh );
+
+$obj->{_writer} = $writer;
+
+###############################################################################
+#
+# Test the _write_xml_declaration() method.
+#
+$caption  = " \tXMLwriter: _write_xml_declaration()";
+$expected = qq(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n);
+
+$obj->_write_xml_declaration();
+
+is( $got, $expected, $caption );
+
+__END__
+
+
