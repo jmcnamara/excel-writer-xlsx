f579f8ede55e8106ab1f28ed6f34b514fbcc79b8
John McNamara <jmcnamara@cpan.org>
2010-09-21 19:07:12 +0100

    Refactored test lib.

--- t/lib/TestFunctions.pm
+++ t/lib/TestFunctions.pm
@@ -16,17 +16,17 @@ use Excel::XLSX::Writer;
 use XML::Writer;
 
 
-our @ISA       = qw(Exporter);
-our @EXPORT    = ();
-our @EXPORT_OK = qw(
+our @ISA         = qw(Exporter);
+our @EXPORT      = ();
+our %EXPORT_TAGS = ();
+our @EXPORT_OK   = qw(
   _expected_to_aref
   _got_to_aref
   _is_deep_diff
   _new_worksheet
   _new_workbook
+  _new_object
 );
-
-our %EXPORT_TAGS = ();
 
 our $VERSION = '0.01';
 
@@ -34,18 +34,21 @@ our $VERSION = '0.01';
 ###############################################################################
 #
 # Turn the embedded XML in the __DATA__ section of the calling test program
-# into an array ref for comparison testing. Also does some data formatting
-# to make comparison easier.
+# into an array ref for comparison testing. Also performs some minor string
+# formatting to make comparison easier with _got_to_aref().
+#
+# The XML data in the testcases is taken from Excel 2007 files with formatting
+# via "xmllint --format".
 #
 sub _expected_to_aref {
 
     my @data;
 
     while ( <main::DATA> ) {
-        next unless /\S/;
         chomp;
-        s{/>$}{ />};
-        s{^\s+}{};
+        next unless /\S/;    # Skip blank lines.
+        s{/>$}{ />};         # Add space before element end like XML::Writer.
+        s{^\s+}{};           # Remove leading whitespace from XML.
         push @data, $_;
     }
 
@@ -55,7 +58,8 @@ sub _expected_to_aref {
 
 ###############################################################################
 #
-# Convert an XML string into an array ref for comparison testing.
+# Convert an XML string returned by the XML::Writer subclasses into an
+# array ref for comparison testing with _expected_to_aref().
 #
 sub _got_to_aref {
 
@@ -72,7 +76,7 @@ sub _got_to_aref {
 
 ###############################################################################
 #
-# Use Test::Differences:: eq_or_diff() where available or else fall back to
+# Use Test::Differences::eq_or_diff() where available or else fall back to
 # using Test::More::is_deeply().
 #
 sub _is_deep_diff {
@@ -95,26 +99,44 @@ sub _is_deep_diff {
 
 ###############################################################################
 #
+# Create a new XML::Writer sub-classed object based on a class name and bind
+# the output to the supplied scalar ref for testing. Calls to the objects XML
+# writing subs will add the output to the scalar.
+#
+sub _new_object {
+
+    my $got_ref = shift;
+    my $class   = shift;
+
+    open my $got_fh, '>', $got_ref or die "Failed to open filehandle: $!";
+
+    my $object = new $class;
+    my $writer = new XML::Writer( OUTPUT => $got_fh );
+
+    $object->{_writer} = $writer;
+
+    return $object;
+}
+
+
+###############################################################################
+#
 # Create a new Worksheet object and bind the output to the supplied scalar ref.
+# This is a special case shorthand for _new_object() because it is used a lot.
 #
 sub _new_worksheet {
 
     my $got_ref = shift;
 
-    open my $got_fh, '>', $got_ref or die "Failed to open filehandle: $!";
-
-    my $worksheet = new Excel::XLSX::Writer::Worksheet;
-    my $writer = new XML::Writer( OUTPUT => $got_fh );
-
-    $worksheet->{_writer} = $writer;
-
-    return $worksheet;
+    return _new_object( $got_ref, 'Excel::XLSX::Writer::Worksheet' );
 }
 
 
 ###############################################################################
 #
 # Create a new Workbook object and bind the output to the supplied scalar ref.
+# This is slightly different than the previous cases since the constructor
+# requires a filename/filehandle.
 #
 sub _new_workbook {
 
