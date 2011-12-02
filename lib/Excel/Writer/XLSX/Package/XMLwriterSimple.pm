93dcf4c30f547097ae58fb72e7d9679f6d87cc5b
John McNamara <jmcnamara@cpan.org>
2011-12-02 00:44:44 +0000

    Prep for release 0.37.

--- lib/Excel/Writer/XLSX/Package/XMLwriterSimple.pm
+++ lib/Excel/Writer/XLSX/Package/XMLwriterSimple.pm
@@ -20,7 +20,7 @@ use Exporter;
 use Carp;
 
 our @ISA     = qw(Exporter);
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 #
 # NOTE: this module is a light weight re-implementation of XML::Writer. See
