58d7a79192898dbf4045b0824f0d8f2916d8d079
John McNamara <jmcnamara@cpan.org>
2015-03-14 21:13:58 +0000

    Prep for release 0.82.

--- lib/Excel/Writer/XLSX.pm
+++ lib/Excel/Writer/XLSX.pm
@@ -18,7 +18,7 @@ use strict;
 use Excel::Writer::XLSX::Workbook;
 
 our @ISA     = qw(Excel::Writer::XLSX::Workbook Exporter);
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 
 
 ###############################################################################
@@ -50,7 +50,7 @@ Excel::Writer::XLSX - Create a new file in the Excel 2007+ XLSX format.
 
 =head1 VERSION
 
-This document refers to version 0.81 of Excel::Writer::XLSX, released November 1, 2014.
+This document refers to version 0.82 of Excel::Writer::XLSX, released March 14, 2015.
 
 
 
--- lib/Excel/Writer/XLSX/Format.pm
+++ lib/Excel/Writer/XLSX/Format.pm
@@ -20,7 +20,7 @@ use Carp;
 
 
 our @ISA     = qw(Exporter);
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 our $AUTOLOAD;
 
 
