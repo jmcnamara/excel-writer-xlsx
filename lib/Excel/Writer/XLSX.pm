cb0c7615bb42c704c665ea3658eff6de2d6ce12f
John McNamara <jmcnamara@cpan.org>
2024-08-02 14:50:52 +0100

    docs: clarify Format set_color() syntax
    Fixes #301

--- lib/Excel/Writer/XLSX.pm
+++ lib/Excel/Writer/XLSX.pm
@@ -3639,7 +3639,7 @@ Set the font size. Excel adjusts the height of a row to accommodate the largest 
 
     Default state:      Excels default color, usually black
     Default action:     Set the default color
-    Valid args:         Integers from 8..63 or the following strings:
+    Valid args:         Html RGB strings like "#FF0000" or the following shortcut strings:
                         'black'
                         'blue'
                         'brown'
